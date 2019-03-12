917e504d99b6c6b3305850274d8a9888714e8023
shunsuke0907 <if.0907@i.softbank.jp>
2019-03-12 21:08:02 +0000

    static_pageの作成完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  # ページごとに条件に応じて整形したタイトルを返す
+  # params string page_title 表示するページのタイトル
+  # return string 整形したタイトル
+  def full_title(page_title = "")
+    base_title = "勤怠管理システム"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
