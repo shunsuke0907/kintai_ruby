4e6607685dd8403a4fac591209ed6de0871172a3
shunsuke0907 <if.0907@i.softbank.jp>
2019-03-27 16:50:35 +0000

    seedファイルに不備があったので修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,5 +16,6 @@ User.create!(name:  "管理者",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               base_id: 1)
 end
