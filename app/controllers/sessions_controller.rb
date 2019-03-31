2ab90887f7d476d7f201f1dbff8146c02baf838b
shunsuke0907 <if.0907@i.softbank.jp>
2019-03-31 12:00:52 +0000

    管理者とそれ以外でそれぞれログイン後の遷移と表示内容を修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_back_or user
+
+      if user.is_admin?
+        flash[:success] = '管理者としてログインしました'
+        redirect_to root_url
+      else
+        flash[:success] = 'ログインしました'
+        redirect_back_or user
+      end
     else
-      flash.now[:danger] = 'メールアドレスとパスワードの情報が一致しませんでした。'
+      flash.now[:danger] = 'メールアドレスとパスワードの情報が一致しませんでした'
       render 'new'
     end
   end
