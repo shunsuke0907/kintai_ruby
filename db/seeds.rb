db1a803c2f0645d81125994a7df0787443c16bf4
shunsuke0907 <if.0907@i.softbank.jp>
2019-03-31 12:20:07 +0000

    seedファイルを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ Base.create!(base_number: "A-1",
 User.create!(name:  "管理者",
              email: "admin@example.com",
              department: 'テスト事業部',
-             employee_number: "admin-01",
-             card_number: 'ADMIN9001',
+             employee_number: "test-1",
+             card_number: 'TEST0001',
              basic_time: '08:00',
              designated_working_start_time: '09:00',
              designated_working_end_time: '18:00',
@@ -15,15 +15,15 @@ User.create!(name:  "管理者",
              is_admin: true,
              base_id: 1)
 
-7.times do |n|
+5.times do |n|
   name  = Faker::Name.name
   email = "supervisor#{n+1}@example.com"
-  card_number = n+1 >= 10 ? "TEST00#{n+1}" : "TEST000#{n+1}";
+  card_number = n+2 >= 10 ? "TEST00#{n+2}" : "TEST000#{n+2}";
   password = 'password'
   User.create!(name:  name,
                email: email,
                department: 'テスト事業部',
-               employee_number: "test-#{n+1}",
+               employee_number: "test-#{n+2}",
                card_number: card_number,
                basic_time: '08:00',
                designated_working_start_time: '09:00',
@@ -34,15 +34,15 @@ User.create!(name:  "管理者",
                base_id: 1)
 end
 
-32.times do |n|
+34.times do |n|
   name  = Faker::Name.name
-  email = "user#{n+8}@example.com"
-  card_number = n+8 >= 10 ? "TEST00#{n+8}" : "TEST000#{n+8}";
+  email = "user#{n+1}@example.com"
+  card_number = n+7 >= 10 ? "TEST00#{n+7}" : "TEST000#{n+7}";
   password = 'password'
   User.create!(name:  name,
                email: email,
                department: 'テスト事業部',
-               employee_number: "test-#{n+8}",
+               employee_number: "test-#{n+7}",
                card_number: card_number,
                basic_time: '08:00',
                designated_working_start_time: '09:00',
